3e738309003d3206ba7e524cd7101bd72ee24178
vinkakniv <vinka.aeris@gmail.com>
2025-02-21 16:53:13 +0700

    revise docker configure for auth service

--- authentication/drizzle.config.ts
+++ authentication/drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import type { Config } from 'drizzle-kit';
 
 export default {
-  schema: './db/schema/**/*.ts',
+  schema: './db/schema/*.ts',
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
