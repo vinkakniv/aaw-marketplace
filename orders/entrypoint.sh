a19c91b1ab32c52400a27358a200bd0754625d25
vinkakniv <vinka.aeris@gmail.com>
2025-02-21 17:08:16 +0700

    configure docker for all services

--- orders/entrypoint.sh
+++ orders/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "Checking dist directory..."
 ls -R dist
+pnpm run migrate
 echo "Migrating"
-node dist/db/migrate.js
+node dist/src/db/migrate.js
 echo "Starting Server"
-node dist/server.js
+node dist/src/server.js
