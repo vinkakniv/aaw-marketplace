b5cffab4550f3595b918106ba77a0747a55525a3
vinkakniv <vinka.aeris@gmail.com>
2025-04-17 14:37:19 +0700

    add configuration for deployment

--- authentication/src/db/index.ts
+++ authentication/src/db/index.ts
@@ -1,14 +1,10 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
-const DB_HOST = process.env.DB_HOST ?? "localhost";
-const DB_PORT = (process.env.DB_PORT as number | undefined) ?? 5432;
-const DB_USER = process.env.DB_USER ?? "postgres"
-const DB_PASSWORD = process.env.DB_PASSWORD ?? "postgres"
-const DB_NAME = process.env.DB_NAME ?? "postgres"
+const DATABASE_URL = process.env.DATABASE_URL ?? "postgres://postgres:postgres@localhost:5432/postgres";
 
 export const pool = new Pool({
-  connectionString: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  connectionString: DATABASE_URL,
 });
 
 export const db = drizzle(pool);
